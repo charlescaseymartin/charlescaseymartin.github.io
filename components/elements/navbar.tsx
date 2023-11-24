7f3019d07d4ca0cfeff0f6af6f168019146a65c8
CharlesCasey23 <charlescaseymartin@gmail.com>
2023-11-24 15:46:19 +0200

    Added mobile navbar toggle

--- components/elements/navbar.tsx
+++ components/elements/navbar.tsx
@@ -24,33 +24,60 @@ export default function Navbar() {
     { text: 'Projects', href: '/projects' },
     { text: 'Blog', href: '/blog' },
   ]
-  const [docElement, setDocElement] = useState<HTMLElement>();
+  const [windowDoc, setWindowDoc] = useState<Document>();
   const appThemeStorgeItem = 'appTheme';
 
-  useEffect(() => {
-    if(document) setDocElement(document.documentElement)
-  }, [])
+
+  useEffect(() => { if (document) setWindowDoc(document) }, []);
 
   const onToggleTheme = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const appTheme = localStorage.getItem(appThemeStorgeItem);
-    if (docElement) {
+    if (windowDoc) {
+      const docElement = windowDoc.documentElement;
+      const themeSwitch = document.querySelector('[data-switch-theme]') as HTMLButtonElement;
+      themeSwitch.children[0].classList.toggle('hidden');
+      themeSwitch.children[1].classList.toggle('hidden');
+
       if (appTheme && appTheme === AppThemeModes.light) {
-        docElement.classList.add(AppThemeModes.dark);
+        docElement.classList.toggle(AppThemeModes.dark);
         localStorage.setItem(appThemeStorgeItem, AppThemeModes.dark);
       }
       if (appTheme && appTheme === AppThemeModes.dark) {
-        docElement.classList.remove(AppThemeModes.dark);
+        docElement.classList.toggle(AppThemeModes.dark);
         localStorage.setItem(appThemeStorgeItem, AppThemeModes.light);
       }
       if (!appTheme && docElement.classList.contains(AppThemeModes.dark)) {
-        docElement.classList.remove(AppThemeModes.dark);
+        docElement.classList.toggle(AppThemeModes.dark);
         localStorage.setItem(appThemeStorgeItem, AppThemeModes.light);
       }
       if (!appTheme && !docElement.classList.contains(AppThemeModes.light)) {
-        docElement.classList.add(AppThemeModes.light);
+        docElement.classList.toggle(AppThemeModes.light);
         localStorage.setItem(appThemeStorgeItem, AppThemeModes.dark);
       }
+    }
+  }
+
+  const onToggleMobileNavbar = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const mobileMenuBtn = event.currentTarget;
+    const mobileNavIsOpen = mobileMenuBtn.getAttribute('data-open-nav') === 'false' ? false : true;
+    const navbar = document.querySelector('[data-navbar]') as HTMLDivElement;
+    const navOverlay = document.querySelector('[data-nav-overlay]') as HTMLDivElement;
+    navbar.classList.toggle('mt-5');
+    
+    if (!mobileNavIsOpen) {
+      mobileMenuBtn.setAttribute('data-open-nav', 'true');
+      navOverlay.setAttribute('data-is-visible', 'true');
+      navbar.style.height = `${navbar.scrollHeight}px`;
+      if (windowDoc) windowDoc.body.classList.toggle('!overflow-y-hidden');
+    }
+
+    if (mobileNavIsOpen) {
+      mobileMenuBtn.setAttribute('data-open-nav', 'false');
+      navOverlay.setAttribute('data-is-visible', 'false');
+      navbar.style.height = '0px';
+      if (windowDoc) windowDoc.body.classList.toggle('!overflow-y-hidden');
     }
   }
 
@@ -66,7 +93,7 @@ export default function Navbar() {
                 <span className='absolute w-4 h-4 -bottom-1 -left-1 bg-primary rounded-md rotate-45'></span>
                 <span className='absolute w-2 h-2 rounded-full bg-heading-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'></span>
               </div>
-              <div className='inline-flex text-lg font-semibold text-heading-1'>
+              <div className='inline-flex text-xl text-heading-1'>
                 charlescaseymartin
               </div>
             </Link>
@@ -86,16 +113,16 @@ export default function Navbar() {
 
           <div className='min-w-max flex items-center gap-x-3'>
             <button data-switch-theme onClick={onToggleTheme} className='outline-none flex relative text-heading-2 rounded-full p-2 lg:p-3 border border-box-border'>
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6 dark:flex hidden'>
+              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6'>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z' />
               </svg>
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6 dark:hidden'>
+              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6 hidden'>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z' />
               </svg>
               <span className='sr-only'>switch theme</span>
             </button>
 
-            <button data-toggle-nav data-open-nav='false' className='lg:hidden lg:invisible outline-none w-7 h-auto flex flex-col relative'>
+            <button data-toggle-nav data-open-nav='false' onClick={onToggleMobileNavbar} className='lg:hidden lg:invisible outline-none w-7 h-auto flex flex-col relative'>
               <span id='line1' className='w-6 h-0.5 rounded-full bg-heading-2 transition-all duration-300 ease-linear'></span>
               <span id='line2' className='w-6 origin-center  mt-1 h-0.5 rounded-ful bg-heading-2 transition-all duration-300 ease-linear'></span>
               <span id='line3' className='w-6 mt-1 h-0.5 rounded-ful bg-heading-2 transition-all duration-300 ease-linear'></span>
